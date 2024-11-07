d72c9042abeccb17542827b58a7374f0a7f65a11
zemise <373933306@qq.com>
2024-11-07 13:49:50 +0800

    update

--- Package.swift
+++ Package.swift
@@ -9,13 +9,18 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FileReader",
-            targets: ["FileReader"]),
+            targets: ["FileReader"]
+        ),
     ],
+
+    dependencies: [],
+
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FileReader"),
+            name: "FileReader"
+            dependencies: []),
         .testTarget(
             name: "FileReaderTests",
             dependencies: ["FileReader"]
